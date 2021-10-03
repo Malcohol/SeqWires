719ee5c19751678187902ac136f34fe3c3f35c90
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2021-10-03 09:13:51 +0100

    Add ChordsFromNotesProcessor.cpp

--- SeqWiresLib/Functions/chordsFromNotesFunction.hpp
+++ SeqWiresLib/Functions/chordsFromNotesFunction.hpp
@@ -5,11 +5,9 @@
  *
  * Licensed under the GPLv3.0. See LICENSE file.
  **/
-
 #include "SeqWiresLib/Tracks/track.hpp"
 
 namespace seqwires {
-
     /// Create tracks of chord events from tracks of note events.
     Track chordsFromNotesFunction(const Track& sourceTrack);
 }
