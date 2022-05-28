11d0b7c1992bf8dd78f7205200e1fe4c344db5f2
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2022-05-28 18:50:32 +0100

    chordMapProcessor now applies

--- SeqWiresLib/Processors/chordMapProcessor.cpp
+++ SeqWiresLib/Processors/chordMapProcessor.cpp
@@ -5,20 +5,26 @@
  *
  * Licensed under the GPLv3.0. See LICENSE file.
  **/
-#include "SeqWiresLib/Processors/chordMapProcessor.hpp"
+#include <SeqWiresLib/Processors/chordMapProcessor.hpp>
 
+#include <SeqWiresLib/Tracks/chordEvents.hpp>
+#include <SeqWiresLib/Tracks/trackEventHolder.hpp>
 #include <SeqWiresLib/chord.hpp>
 
-#include "BabelWiresLib/Features/mapFeature.hpp"
+#include <BabelWiresLib/Features/mapFeature.hpp>
+#include <BabelWiresLib/Features/rootFeature.hpp>
+#include <BabelWiresLib/Maps/Helpers/enumValueAdapters.hpp>
+#include <BabelWiresLib/Maps/Helpers/unorderedMapApplicator.hpp>
+#include <BabelWiresLib/Project/projectContext.hpp>
+#include <BabelWiresLib/TypeSystem/typeSystem.hpp>
 
-#include "Common/Identifiers/registeredIdentifier.hpp"
+#include <Common/Identifiers/registeredIdentifier.hpp>
 
 namespace {
     struct ChordTypeMap : babelwires::MapFeature {
         ChordTypeMap()
-            : babelwires::MapFeature(
-                  babelwires::TypeIdSet{seqwires::ChordType::getThisIdentifier()},
-                  babelwires::TypeIdSet{seqwires::ChordType::getThisIdentifier()}) {}
+            : babelwires::MapFeature(babelwires::TypeIdSet{seqwires::ChordType::getThisIdentifier()},
+                                     babelwires::TypeIdSet{seqwires::ChordType::getThisIdentifier()}) {}
     };
 } // namespace
 
@@ -34,4 +40,32 @@ seqwires::ChordMapProcessor::Factory::Factory()
           REGISTERED_LONGID("ChordMapProcessor", "Chord Map", "b7227130-8274-4451-bd60-8fe34a74c4b6"), 1) {}
 
 void seqwires::ChordMapProcessor::processEntry(babelwires::UserLogger& userLogger, const seqwires::TrackFeature& input,
-                                               seqwires::TrackFeature& output) const {}
+                                               seqwires::TrackFeature& output) const {
+    const babelwires::MapData& mapData = m_chordTypeMapFeature->get();
+    const babelwires::ProjectContext& context = babelwires::RootFeature::getProjectContextAt(*m_chordTypeMapFeature);
+    if (!mapData.isValid(context)) {
+        throw babelwires::ModelException() << "The Map is not valid.";
+    }
+
+    const ChordType& chordType =
+        context.m_typeSystem.getEntryByIdentifier(seqwires::ChordType::getThisIdentifier())->is<ChordType>();
+
+    const babelwires::EnumToIndexValueAdapter adapter{chordType};
+
+    babelwires::UnorderedMapApplicator<unsigned int, unsigned int> applicator(mapData, adapter, adapter);
+
+    const Track& trackIn = input.get();
+    Track trackOut;
+
+    for (auto it = trackIn.begin(); it != trackIn.end(); ++it) {
+        if (it->as<ChordOnEvent>()) {
+            TrackEventHolder holder(*it);
+            ChordType::Value& type = holder->is<ChordOnEvent>().m_chord.m_chordType;
+            type = static_cast<ChordType::Value>(applicator[static_cast<unsigned int>(type)]);
+            trackOut.addEvent(holder.release());
+        } else {
+            trackOut.addEvent(*it);
+        }
+    }
+    trackOut.setDuration(trackIn.getDuration());
+}
