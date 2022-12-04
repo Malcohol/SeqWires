069083791d4ec2ab9662c5f9832b2663a3c07633
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2022-12-04 19:35:53 +0000

    Redo mapPercussionFunction using instruments

--- SeqWiresLib/Functions/percussionMapFunction.cpp
+++ SeqWiresLib/Functions/percussionMapFunction.cpp
@@ -7,34 +7,16 @@
  **/
 #include <SeqWiresLib/Functions/percussionMapFunction.hpp>
 
-#include <SeqWiresLib/Tracks/noteEvents.hpp>
+#include <SeqWiresLib/Tracks/percussionEvents.hpp>
 #include <SeqWiresLib/Tracks/trackEventHolder.hpp>
 #include <SeqWiresLib/percussion.hpp>
 
 #include <BabelWiresLib/Features/mapFeature.hpp>
 #include <BabelWiresLib/Features/modelExceptions.hpp>
 #include <BabelWiresLib/Maps/Helpers/enumSourceMapApplicator.hpp>
+#include <BabelWiresLib/Maps/Helpers/unorderedMapApplicator.hpp>
 #include <BabelWiresLib/Maps/Helpers/enumValueAdapters.hpp>
 #include <BabelWiresLib/TypeSystem/typeSystem.hpp>
-
-namespace {
-    using MapType = babelwires::EnumSourceMapApplicator<seqwires::GM2StandardPercussionKit, seqwires::GM2StandardPercussionKit::Value>;
-
-    bool mapPercussionEvent(const seqwires::GM2StandardPercussionKit& percussionType, MapType& mapApplicator,
-                            seqwires::NoteEvent& noteEvent) {
-        const seqwires::Pitch originalPitch = noteEvent.getPitch();
-        seqwires::GM2StandardPercussionKit::Value originalValue;
-        if (percussionType.tryGetValueFromPitch(originalPitch, originalValue)) {
-            const seqwires::GM2StandardPercussionKit::Value newValue = mapApplicator[originalValue];
-            const seqwires::Pitch newPitch = seqwires::GM2StandardPercussionKit::getPitchFromValue(newValue);
-            noteEvent.setPitch(newPitch);
-            return true;
-        }
-        // Not a known percussion event.
-        // TODO: Probably would be better to use default here.
-        return false;
-    }
-} // namespace
 
 seqwires::Track seqwires::mapPercussionFunction(const babelwires::TypeSystem& typeSystem, const Track& trackIn,
                                                 const babelwires::MapData& percussionMapData) {
@@ -42,11 +24,14 @@ seqwires::Track seqwires::mapPercussionFunction(const babelwires::TypeSystem& ty
     if (!percussionMapData.isValid(typeSystem)) {
         throw babelwires::ModelException() << "The Percussion Map is not valid.";
     }
-    const GM2StandardPercussionKit& percussionType =
-        typeSystem.getEntryByIdentifier(seqwires::GM2StandardPercussionKit::getThisIdentifier())->is<GM2StandardPercussionKit>();
 
-    const babelwires::EnumToValueValueAdapter<GM2StandardPercussionKit> targetAdapter{percussionType};
-    MapType mapApplicator(percussionMapData, percussionType, targetAdapter);
+    //const PercussionKit& sourceType = typeSystem.getEntryByIdentifier(percussionMapData.getSourceTypeId())->is<PercussionKit>();
+    //const PercussionKit& targetType = typeSystem.getEntryByIdentifier(percussionMapData.getTargetTypeId())->is<PercussionKit>();
+
+    const babelwires::EnumToIdentifierValueAdapter sourceAdapter;
+    const babelwires::EnumToIdentifierValueAdapter targetAdapter;
+
+    babelwires::UnorderedMapApplicator<babelwires::Identifier, babelwires::Identifier> mapApplicator{ percussionMapData, sourceAdapter, targetAdapter };
 
     Track trackOut;
     // If an event is dropped, then we need to carry its time forward for the next event.
@@ -54,15 +39,18 @@ seqwires::Track seqwires::mapPercussionFunction(const babelwires::TypeSystem& ty
 
     for (auto it = trackIn.begin(); it != trackIn.end(); ++it) {
         const TrackEvent::GroupingInfo info = it->getGroupingInfo();
-        if (info.m_category == NoteEvent::s_noteEventCategory) {
+        if (info.m_category == PercussionEvent::s_percussionEventCategory) {
             TrackEventHolder holder(*it);
-            if (mapPercussionEvent(percussionType, mapApplicator, static_cast<NoteEvent&>(*holder))) {
-                holder->setTimeSinceLastEvent(holder->getTimeSinceLastEvent() + timeFromDroppedEvent);
-                timeFromDroppedEvent = 0;
-                trackOut.addEvent(holder.release());
-            } else {
-                timeFromDroppedEvent += it->getTimeSinceLastEvent();
-            }
+            PercussionEvent& percussionEvent = static_cast<PercussionEvent&>(*holder);
+            babelwires::Identifier newInstrument = mapApplicator[percussionEvent.getInstrument()];
+            // TODO if (newInstrument != "DISCARD")
+            percussionEvent.setInstrument(newInstrument);
+            percussionEvent.setTimeSinceLastEvent(holder->getTimeSinceLastEvent() + timeFromDroppedEvent);
+            timeFromDroppedEvent = 0;
+            trackOut.addEvent(holder.release());
+            //} else {
+            //    timeFromDroppedEvent += it->getTimeSinceLastEvent();
+            //}
         } else if (timeFromDroppedEvent > 0) {
             TrackEventHolder holder(*it);
             holder->setTimeSinceLastEvent(holder->getTimeSinceLastEvent() + timeFromDroppedEvent);
