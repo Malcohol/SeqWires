c9e765feef399ee63bed515b49846deb15ce0581
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2023-02-05 18:56:59 +0000

    Rename Plugins folder

---
+++ Plugins/Smf/Plugin/smfFormat.cpp
@@ -0,0 +1,65 @@
+/**
+ * Formats which support Standard MIDI Files.
+ *
+ * (C) 2021 Malcolm Tyrrell
+ *
+ * Licensed under the GPLv3.0. See LICENSE file.
+ **/
+#include <Plugins/Smf/Plugin/smfFormat.hpp>
+
+#include <Plugins/Smf/Plugin/smfParser.hpp>
+#include <Plugins/Smf/Plugin/smfWriter.hpp>
+
+namespace {
+
+    const char s_formatIdentifier[] = "StandardMIDIFile";
+    const char s_manufacturerName[] = "MIDI Manufacturers Association";
+    const char s_productName[] = "Standard MIDI File";
+
+} // namespace
+
+smf::SmfSourceFormat::SmfSourceFormat()
+    : SourceFileFormat(
+          REGISTERED_LONGID(s_formatIdentifier, "Standard MIDI file (in)", "418b8238-c184-4885-a369-b24c4e0d06ec"), 1,
+          Extensions{"mid", "smf"}) {}
+
+babelwires::LongIdentifier smf::SmfSourceFormat::getThisIdentifier() {
+    return s_formatIdentifier;
+}
+
+std::string smf::SmfSourceFormat::getManufacturerName() const {
+    return s_manufacturerName;
+}
+
+std::string smf::SmfSourceFormat::getProductName() const {
+    return s_productName;
+}
+
+std::unique_ptr<babelwires::FileFeature>
+smf::SmfSourceFormat::loadFromFile(babelwires::DataSource& dataSource, const babelwires::ProjectContext& projectContext,
+                                   babelwires::UserLogger& userLogger) const {
+    return parseSmfSequence(dataSource, projectContext, userLogger);
+}
+
+smf::SmfTargetFormat::SmfTargetFormat()
+    : TargetFileFormat(REGISTERED_LONGID("SmfFile", "Standard MIDI file (out)", "f29cd3b0-8a46-4a21-bb7d-53acd6702944"),
+                       1, Extensions{"mid", "smf"}) {}
+
+std::string smf::SmfTargetFormat::getManufacturerName() const {
+    return s_manufacturerName;
+}
+
+std::string smf::SmfTargetFormat::getProductName() const {
+    return s_productName;
+}
+
+std::unique_ptr<babelwires::FileFeature>
+smf::SmfTargetFormat::createNewFeature(const babelwires::ProjectContext& projectContext) const {
+    return std::make_unique<target::SmfFeature>(projectContext);
+}
+
+void smf::SmfTargetFormat::writeToFile(const babelwires::ProjectContext& projectContext,
+                                       babelwires::UserLogger& userLogger, const babelwires::FileFeature& sequence,
+                                       std::ostream& os) const {
+    writeToSmf(projectContext, userLogger, sequence.as<target::SmfFeature>()->getFormatFeature(), os);
+}
---
+++ Plugins/Smf/Plugin/smfTargetModel.cpp
@@ -0,0 +1,94 @@
+/**
+ * Representation of a Standard MIDI File as a tree of Features.
+ *
+ * (C) 2021 Malcolm Tyrrell
+ *
+ * Licensed under the GPLv3.0. See LICENSE file.
+ **/
+#include <Plugins/Smf/Plugin/smfTargetModel.hpp>
+
+#include <Plugins/Smf/Plugin/smfFormat.hpp>
+
+#include <BabelWiresLib/Features/featureMixins.hpp>
+
+#include <SeqWiresLib/Features/trackFeature.hpp>
+
+#include <Common/Identifiers/registeredIdentifier.hpp>
+
+smf::target::ChannelTrackFeature::ChannelTrackFeature() {
+    m_channelNum = addField(std::make_unique<babelwires::HasStaticRange<babelwires::IntFeature, 0, 15>>(),
+                            REGISTERED_ID("Chan", "channel", "cad592ef-2355-4837-a265-38b49eae7599"));
+    m_trackFeature = addField(std::make_unique<seqwires::TrackFeature>(),
+                              REGISTERED_ID("Track", "track", "a6db15c9-9f29-4fb3-92c4-771746b2b97f"));
+}
+
+int smf::target::ChannelTrackFeature::getChannelNumber() const {
+    return m_channelNum->get();
+}
+
+const seqwires::Track& smf::target::ChannelTrackFeature::getTrack() const {
+    return m_trackFeature->get();
+}
+
+
+int smf::target::ArrayChannelGroup::getNumTracks() const {
+    return getNumFeatures();
+}
+
+const smf::target::ChannelTrackFeature& smf::target::ArrayChannelGroup::getTrack(int i) const {
+    return static_cast<const ChannelTrackFeature&>(*getFeature(i));
+}
+
+std::unique_ptr<babelwires::Feature> smf::target::ArrayChannelGroup::createNextEntry() const {
+    return std::make_unique<ChannelTrackFeature>();
+}
+
+babelwires::Range<unsigned int> smf::target::ArrayChannelGroup::doGetSizeRange() const {
+    auto defaultRange = babelwires::ArrayFeature::doGetSizeRange();
+    defaultRange.m_min = 1;
+    return defaultRange;
+}
+
+namespace {
+    const babelwires::IdentifiersSource formatIdentifiersSource = {
+        {"SMF0", "0", "82835685-a66e-417e-b076-5d706d03a989"}, {"SMF1", "1", "ce16e042-445b-423d-a493-874ed4c238b3"}};
+}
+
+smf::target::SmfFormatFeature::SmfFormatFeature()
+    : UnionFeature(REGISTERED_ID_VECTOR(formatIdentifiersSource), 0) {
+    m_metadata = addField(std::make_unique<MidiMetadata>(),
+                          REGISTERED_ID("Meta", "Metadata", "72bbbcee-2b53-4fb2-bfb8-4f5e495f9166"));
+    m_channelGroup = addField(std::make_unique<ArrayChannelGroup>(),
+                              REGISTERED_ID("tracks", "tracks", "1aa014da-2fb3-46e2-96df-98c7798b4a87"));
+}
+
+const smf::MidiMetadata& smf::target::SmfFormatFeature::getMidiMetadata() const {
+    return *m_metadata;
+}
+
+smf::MidiMetadata& smf::target::SmfFormatFeature::getMidiMetadata() {
+    return *m_metadata;
+}
+
+smf::target::SmfFeature::SmfFeature(const babelwires::ProjectContext& projectContext)
+    : babelwires::FileFeature(projectContext, SmfSourceFormat::getThisIdentifier()) {
+    m_formatFeature = addField(std::make_unique<SmfFormatFeature>(),
+                               REGISTERED_ID("Format", "Format", "1f2eaefb-b48d-484d-8793-e14f2fa0193b"));
+}
+
+int smf::target::SmfFormatFeature::getNumMidiTracks() const {
+    return m_channelGroup->getNumTracks();
+}
+
+const smf::target::ChannelTrackFeature& smf::target::SmfFormatFeature::getMidiTrack(int i) const {
+    return m_channelGroup->getTrack(i);
+}
+
+const smf::target::SmfFormatFeature& smf::target::SmfFeature::getFormatFeature() const {
+    return *m_formatFeature;
+}
+
+babelwires::Feature::Style smf::target::SmfFormatFeature::getStyle() const {
+    // Not collapsable and inlined.
+    return babelwires::Feature::Style::isInlined;
+}
