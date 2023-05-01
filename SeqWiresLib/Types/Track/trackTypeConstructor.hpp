be9c62de8a373a7734898f4ce4c34ae7e8526256
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2023-05-01 16:24:21 +0100

    Provide TypeSystem as a parameter to constructType

--- SeqWiresLib/Types/Track/trackTypeConstructor.hpp
+++ SeqWiresLib/Types/Track/trackTypeConstructor.hpp
@@ -19,7 +19,7 @@ namespace seqwires {
         /// Note that the we don't represent the default in the name.
         TYPE_CONSTRUCTOR("Track", "Track", "bba2f18c-9ff1-406c-b39c-47a2e4047caa", 1);
 
-        std::unique_ptr<babelwires::Type> constructType(babelwires::TypeRef newTypeRef, const std::vector<const babelwires::Type*>& typeArguments,
+        std::unique_ptr<babelwires::Type> constructType(const babelwires::TypeSystem& typeSystem, babelwires::TypeRef newTypeRef, const std::vector<const babelwires::Type*>& typeArguments,
                                             const std::vector<babelwires::EditableValueHolder>& valueArguments) const override;
 
         babelwires::SubtypeOrder compareSubtypeHelper(const babelwires::TypeSystem& typeSystem, const babelwires::TypeConstructorArguments& argumentsA,
