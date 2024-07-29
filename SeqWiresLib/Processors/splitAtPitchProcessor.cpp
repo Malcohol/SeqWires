f8363e5cad410db0b99421a2df732331c912c92f
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2024-07-29 16:56:09 +0100

    First sketch of ValueProcessor using Instance

--- SeqWiresLib/Processors/splitAtPitchProcessor.cpp
+++ SeqWiresLib/Processors/splitAtPitchProcessor.cpp
@@ -1,43 +1,47 @@
 /**
- * A processor which repeats sequence data a number of times. 
- * 
+ * A processor which repeats sequence data a number of times.
+ *
  * (C) 2021 Malcolm Tyrrell
- * 
+ *
  * Licensed under the GPLv3.0. See LICENSE file.
  **/
 #include <SeqWiresLib/Processors/splitAtPitchProcessor.hpp>
 
-#include <SeqWiresLib/Types/Track/trackFeature.hpp>
 #include <SeqWiresLib/Functions/splitAtPitchFunction.hpp>
+#include <SeqWiresLib/Types/Track/trackFeature.hpp>
 
 #include <BabelWiresLib/Features/arrayFeature.hpp>
-#include <BabelWiresLib/Types/Enum/enumFeature.hpp>
-#include <BabelWiresLib/Features/rootFeature.hpp>
 #include <BabelWiresLib/Features/modelExceptions.hpp>
+#include <BabelWiresLib/Features/rootFeature.hpp>
+#include <BabelWiresLib/Types/Enum/enumFeature.hpp>
 
 #include <Common/Identifiers/registeredIdentifier.hpp>
 
 #include <set>
 
 seqwires::SplitAtPitchProcessor::SplitAtPitchProcessor(const babelwires::ProjectContext& projectContext)
-: CommonProcessor(projectContext) {
-    m_pitch = m_inputFeature->addField(std::make_unique<babelwires::EnumFeature>(seqwires::PitchEnum::getThisIdentifier()),
-                                       BW_SHORT_ID("Pitch", "Pitch", "6b721baa-084f-450b-bf35-2e08a9592958"));
+    : CommonProcessor(projectContext) {
+    m_pitch =
+        m_inputFeature->addField(std::make_unique<babelwires::EnumFeature>(seqwires::PitchEnum::getThisIdentifier()),
+                                 BW_SHORT_ID("Pitch", "Pitch", "6b721baa-084f-450b-bf35-2e08a9592958"));
     m_trackIn = m_inputFeature->addField(std::make_unique<TrackFeature>(),
-                                              BW_SHORT_ID("Input", "Input Track", "9314a43f-256a-4915-b218-f2ba37133863"));
-    m_equalOrAboveTrackOut = m_outputFeature->addField(std::make_unique<TrackFeature>(),
-                                                BW_SHORT_ID("Above", "Notes at/above", "4c65b7f1-e546-4df2-9891-23623b74bc23"));
-    m_belowTrackOut = m_outputFeature->addField(std::make_unique<TrackFeature>(),
-                                                BW_SHORT_ID("Below", "Notes below", "ab6a1d57-8c77-4df9-baf7-4b24136d9279"));
+                                         BW_SHORT_ID("Input", "Input Track", "9314a43f-256a-4915-b218-f2ba37133863"));
+    m_equalOrAboveTrackOut =
+        m_outputFeature->addField(std::make_unique<TrackFeature>(),
+                                  BW_SHORT_ID("Above", "Notes at/above", "4c65b7f1-e546-4df2-9891-23623b74bc23"));
+    m_belowTrackOut = m_outputFeature->addField(
+        std::make_unique<TrackFeature>(), BW_SHORT_ID("Below", "Notes below", "ab6a1d57-8c77-4df9-baf7-4b24136d9279"));
     m_otherTrackOut = m_outputFeature->addField(std::make_unique<TrackFeature>(),
                                                 BW_SHORT_ID("Other", "Other", "83bf663c-2931-467f-8403-b12e18138c68"));
 }
 
 seqwires::SplitAtPitchProcessor::Factory::Factory()
-    : CommonProcessorFactory(BW_LONG_ID("SplitAtPitchProcessor", "Split At Pitch", "9f5076f5-42a1-4655-a71e-b6b06a4e92cc"), 1) {}
+    : CommonProcessorFactory(
+          BW_LONG_ID("SplitAtPitchProcessor", "Split At Pitch", "9f5076f5-42a1-4655-a71e-b6b06a4e92cc"), 1) {}
 
 void seqwires::SplitAtPitchProcessor::process(babelwires::UserLogger& userLogger) {
-    if (m_pitch->isChanged(babelwires::Feature::Changes::SomethingChanged) || m_trackIn->isChanged(babelwires::Feature::Changes::SomethingChanged)) {
+    if (m_pitch->isChanged(babelwires::Feature::Changes::SomethingChanged) ||
+        m_trackIn->isChanged(babelwires::Feature::Changes::SomethingChanged)) {
         const int pitch = m_pitch->tryGetEnumIndex();
         if (pitch >= 0) {
             auto newTracksOut = splitAtPitch(Pitch(pitch), m_trackIn->get());
@@ -49,3 +53,23 @@ void seqwires::SplitAtPitchProcessor::process(babelwires::UserLogger& userLogger
         }
     }
 }
+
+seqwires::SplitAtPitchProcessorInput::SplitAtPitchProcessorInput()
+    : babelwires::RecordType({{BW_SHORT_ID("Pitch", "Pitch", "6b721baa-084f-450b-bf35-2e08a9592958"),
+                               seqwires::PitchEnum::getThisIdentifier()},
+                              {BW_SHORT_ID("Input", "Input Track", "9314a43f-256a-4915-b218-f2ba37133863"),
+                               DefaultTrackType::getThisIdentifier()}}) {}
+
+seqwires::SplitAtPitchProcessorOutput::SplitAtPitchProcessorOutput()
+    : babelwires::RecordType({
+          {BW_SHORT_ID("Above", "Notes at/above", "4c65b7f1-e546-4df2-9891-23623b74bc23"),
+           DefaultTrackType::getThisIdentifier()},
+          {BW_SHORT_ID("Below", "Notes below", "ab6a1d57-8c77-4df9-baf7-4b24136d9279"),
+           DefaultTrackType::getThisIdentifier()},
+          {BW_SHORT_ID("Other", "Other", "83bf663c-2931-467f-8403-b12e18138c68"),
+           DefaultTrackType::getThisIdentifier()},
+      }) {}
+
+seqwires::SplitAtPitchProcessor2::SplitAtPitchProcessor2(const babelwires::ProjectContext& projectContext)
+    : ValueProcessor(projectContext) {
+    }
--- SeqWiresLib/Processors/splitAtPitchProcessor.hpp
+++ SeqWiresLib/Processors/splitAtPitchProcessor.hpp
@@ -7,7 +7,15 @@
  **/
 #pragma once
 
+#include <SeqWiresLib/pitch.hpp>
+#include <SeqWiresLib/Types/Track/trackType.hpp>
+#include <SeqWiresLib/Types/Track/trackInstance.hpp>
+
 #include <BabelWiresLib/Processors/commonProcessor.hpp>
+#include <BabelWiresLib/TypeSystem/primitiveType.hpp>
+#include <BabelWiresLib/Types/Record/recordType.hpp>
+#include <BabelWiresLib/Instance/instance.hpp>
+#include <BabelWiresLib/Processors/valueProcessor.hpp>
 
 namespace babelwires {
     class EnumFeature;
@@ -16,7 +24,7 @@ namespace babelwires {
 namespace seqwires {
     class TrackFeature;
 
-    /// A processor which limits a track to events between certain points.
+    /// A processor which limits a track to events between certain pitches.
     class SplitAtPitchProcessor : public babelwires::CommonProcessor {
       public:
         SplitAtPitchProcessor(const babelwires::ProjectContext& projectContext);
@@ -32,6 +40,40 @@ namespace seqwires {
         TrackFeature* m_equalOrAboveTrackOut;
         TrackFeature* m_belowTrackOut;
         TrackFeature* m_otherTrackOut;
+    };
+
+    class SplitAtPitchProcessorInput : babelwires::RecordType {
+      public:
+        PRIMITIVE_TYPE("PitchSplitIn", "Split At Pitch Input", "f901af3a-c27b-449c-961a-8f43dee7d9a6", 1);
+
+        SplitAtPitchProcessorInput();
+
+        DECLARE_INSTANCE_BEGIN(SplitAtPitchProcessorInput)
+        DECLARE_INSTANCE_FIELD(Pitch, seqwires::PitchEnum)
+        DECLARE_INSTANCE_FIELD(Input, seqwires::TrackType)
+        DECLARE_INSTANCE_END()        
+    };
+
+    class SplitAtPitchProcessorOutput : babelwires::RecordType {
+      public:
+        PRIMITIVE_TYPE("PitchSplitOut", "Split At Pitch Output", "50f790e1-0ef3-4f34-ad14-5a6762772e19", 1);
+
+        SplitAtPitchProcessorOutput();
+
+        DECLARE_INSTANCE_BEGIN(SplitAtPitchProcessorInput)
+        DECLARE_INSTANCE_FIELD(Above, seqwires::TrackType)
+        DECLARE_INSTANCE_FIELD(Below, seqwires::TrackType)
+        DECLARE_INSTANCE_FIELD(Other, seqwires::TrackType)
+        DECLARE_INSTANCE_END()
+    };
+
+    class SplitAtPitchProcessor2 : public babelwires::ValueProcessor<SplitAtPitchProcessorInput, SplitAtPitchProcessorOutput> {
+      public:
+        SplitAtPitchProcessor2(const babelwires::ProjectContext& projectContext);
+
+      protected:
+        void processValue(babelwires::UserLogger& userLogger, const babelwires::Instance<const babelwires::ValueFeature, SplitAtPitchProcessorInput>& input,
+                                  babelwires::Instance<babelwires::ValueFeature, SplitAtPitchProcessorOutput>& output) const override;
     };
 
 } // namespace seqwires
