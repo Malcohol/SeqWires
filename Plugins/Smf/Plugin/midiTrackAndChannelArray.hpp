a5c4a8aa6bf0ebeff834b44e0fb29fcd56da27cd
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2024-08-04 14:46:36 +0100

    Comment

--- Plugins/Smf/Plugin/midiTrackAndChannelArray.hpp
+++ Plugins/Smf/Plugin/midiTrackAndChannelArray.hpp
@@ -13,7 +13,8 @@
 #include <Plugins/Smf/Plugin/midiTrackAndChannel.hpp>
 
 namespace smf {
-    // an array of MidiTrackAndChannel records.
+    // An array of MidiTrackAndChannel records.
+    // TODO: Just use a type constructor for this rather than a C++ class?
     class MidiTrackAndChannelArray : public babelwires::ArrayType {
       public:
         PRIMITIVE_TYPE("MidiTrackArray", "MIDI Track and Channels", "b5462658-be68-4570-b0a5-d2dcb03f4d8e", 1);
